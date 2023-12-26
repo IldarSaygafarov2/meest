6cdf2dd53abd824e5f71c70d330f69cd905017f6
sayildar <sayildar17@gmail.com>
2023-12-26 12:28:02 +0500

    ade

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,23 +11,31 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hn&*^m+%sfhco=2402#muhdgto4k&35eh$2k5!iqm#hnlrf%m9"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
     'meest.pythonanywhere.com',
-    '127.0.0.1'
+    '127.0.0.1',
+    '185.217.131.80'
 ]
 
 
@@ -80,9 +88,13 @@ WSGI_APPLICATION = "core.core.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': 5432
     }
 }
 
--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
-
 bind = "127.0.0.1:8000"
 workers = 2 * multiprocessing.cpu_count() + 1
 user = "ildus"
 timeout = 120
+raw_env = ["DJANGO_SETTINGS_MODULE=core.core.settings"]
