b370a2ecb588b7f733ef4bc9ac139b10c24697d8
sayildar <sayildar17@gmail.com>
2023-12-26 12:36:45 +0500

    cls

--- core/settings.py
+++ core/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "core.core.urls"
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
@@ -81,7 +81,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "core.core.wsgi.application"
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -4,4 +4,4 @@ bind = "127.0.0.1:8000"
 workers = 2 * multiprocessing.cpu_count() + 1
 user = "ildus"
 timeout = 120
-raw_env = ["DJANGO_SETTINGS_MODULE=core.core.settings"]
+raw_env = ["DJANGO_SETTINGS_MODULE=core.settings"]
